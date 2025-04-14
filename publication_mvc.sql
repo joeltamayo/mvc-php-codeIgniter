afe8c14a8018dcf027a8be6c9030443778c54d7b
joel tamayo <jueltamayo25@gmail.com>
2025-04-14 01:31:17 -0600

    Actualizacion base de datos

--- publication_mvc.sql
+++ publication_mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-04-2025 a las 00:16:37
+-- Tiempo de generación: 14-04-2025 a las 09:30:04
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,9 +40,22 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`id`, `user_id`, `filename`, `uploaded_at`, `ordering`) VALUES
-(5, 12, 'uploads/gallery/1744491430_68876c87cc98d068d185.jpg', '2025-04-13 02:57:10', 0),
-(10, 9, 'uploads/gallery/1744494227_81cae82006cc7d91866e.png', '2025-04-13 03:43:47', 0),
-(11, 9, 'uploads/gallery/1744494316_08aff668dec84e474b9d.jpg', '2025-04-13 03:45:16', 0);
+(18, 8, 'uploads/gallery/1744599155_44b535793ff5350d3843.png', '2025-04-14 08:52:35', 0),
+(19, 8, 'uploads/gallery/1744599163_23f30e954e8f56c5a6f1.jpg', '2025-04-14 08:52:43', 0),
+(20, 8, 'uploads/gallery/1744599183_b7e1ddc0cd8d3b889f46.jpg', '2025-04-14 08:53:03', 0),
+(21, 8, 'uploads/gallery/1744599197_0560c2f55b21bc72d85b.jpg', '2025-04-14 08:53:17', 0),
+(22, 12, 'uploads/gallery/1744599225_fbcd33e4bb207bd1d84f.jpg', '2025-04-14 08:53:45', 0),
+(23, 12, 'uploads/gallery/1744599232_aec1e1d390ee3f2abb5e.jpg', '2025-04-14 08:53:52', 0),
+(24, 12, 'uploads/gallery/1744599240_bf297e22914accb81796.jpg', '2025-04-14 08:54:00', 0),
+(25, 12, 'uploads/gallery/1744599259_94a325a1121cdb3304f7.jpg', '2025-04-14 08:54:19', 0),
+(26, 16, 'uploads/gallery/1744599289_ebe0f8e25e1baf914966.jpg', '2025-04-14 08:54:49', 0),
+(27, 16, 'uploads/gallery/1744599296_694949fd008c84cbad12.jpg', '2025-04-14 08:54:56', 0),
+(28, 16, 'uploads/gallery/1744599305_73bda351275b63746b46.jpg', '2025-04-14 08:55:05', 0),
+(29, 19, 'uploads/gallery/1744599347_31a1e3e5dd846aec745d.jpg', '2025-04-14 08:55:47', 0),
+(30, 19, 'uploads/gallery/1744599355_af92ac82aa467793039d.jpg', '2025-04-14 08:55:55', 0),
+(31, 8, 'uploads/gallery/1744599379_be7b53b4e66254720b27.jpg', '2025-04-14 08:56:19', 0),
+(32, 8, 'uploads/gallery/1744599386_008cd93430e5523ad708.jpg', '2025-04-14 08:56:26', 0),
+(33, 8, 'uploads/gallery/1744599396_2917756998f8add6d0d2.png', '2025-04-14 08:56:36', 0);
 
 -- --------------------------------------------------------
 
@@ -106,11 +119,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`, `role`) VALUES
-(8, 'pedro 2.1', 'pedro@gmail.com', '1234', '2025-04-09 09:12:02', '2025-04-12 16:43:26', 'user'),
-(9, 'joel tamayo 2', 'joel@gmail.com', '12345', '2025-04-09 13:03:45', '2025-04-12 16:51:37', 'admin'),
-(10, 'juan', 'juan@gmail.com', 'juan123', '2025-04-09 13:17:53', '2025-04-09 13:17:53', 'user'),
-(11, 'pablo 2', 'pablo@gmail.com', '1234', '2025-04-12 14:57:24', '2025-04-12 14:57:40', 'admin'),
-(12, 'sancho 2', 'sancho@gmail.com', '1234', '2025-04-12 14:58:35', '2025-04-12 16:44:00', 'user');
+(8, 'Pedro Alvarez Garza', 'pedro@gmail.com', '1234', '2025-04-09 09:12:02', '2025-04-14 08:23:02', 'user'),
+(9, 'Joel Tamayo de Anda', 'joel@gmail.com', '12345', '2025-04-09 13:03:45', '2025-04-14 08:23:28', 'admin'),
+(12, 'Alex Gerardo', 'alex@gmail.com', '1234', '2025-04-12 14:58:35', '2025-04-14 08:25:35', 'user'),
+(16, 'Javier Alberto Guetierrez Hernandez', 'javier@gmail.com', '1234', '2025-04-14 07:55:24', '2025-04-14 08:25:08', 'user'),
+(19, 'Alberto Alarcon', 'alberto@gmail.com', '1234', '2025-04-14 08:38:39', '2025-04-14 10:22:49', 'user');
 
 --
 -- Índices para tablas volcadas
@@ -148,7 +161,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de la tabla `publication`
@@ -166,7 +179,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
